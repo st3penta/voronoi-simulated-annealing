86c2cbc53241277721b601b53653b3ea07a15059
st3penta <71923697+st3penta@users.noreply.github.com>
2023-02-25 14:25:22 +0100

    chore: polish main.go file

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 )
 
 var (
+	// defaults argument values for the `run` command
 	defaultNumSeeds           = 50
 	defaultSimulationDuration = 3 * time.Hour
 	defaultSnapshotsInterval  = 1 * time.Minute
@@ -22,6 +23,9 @@ var (
 
 func main() {
 
+	//
+	// CLI initialization
+	//
 	var numSeeds int
 	var inputImageFilePath string
 	var simulationDuration time.Duration
@@ -41,7 +45,7 @@ func main() {
 			&cli.StringFlag{
 				Name:        "targetImage",
 				Aliases:     []string{"i"},
-				Usage:       "Path to the input image `FILE` to be used as target image for the annealing",
+				Usage:       "Path to the input image `FILE` to be used as target image for the annealing. Only JPG images are supported",
 				Value:       "./res/" + defaultImageName + ".jpg",
 				Destination: &inputImageFilePath,
 			},
@@ -88,29 +92,35 @@ func main() {
 		},
 	}
 
+	// run the cli
 	if err := app.Run(os.Args); err != nil {
 		panic(err)
 	}
 }
 
+// getTargetImage reads the target image at the specified path, and extracts the RGB values of each pixel
 func getTargetImage(inputImageFilePath string) TargetImage {
 
+	// get file name stripped from path and extension
 	fileNameWithExt := filepath.Base(inputImageFilePath)
 	fileExtension := filepath.Ext(inputImageFilePath)
 	fileName := strings.Replace(fileNameWithExt, fileExtension, "", 1)
 
+	// open the image file
 	reader, openErr := os.Open(inputImageFilePath)
 	if openErr != nil {
 		panic(openErr)
 	}
 	defer reader.Close()
 
+	// decode the image using the JPG decoder
 	image, _, decodeErr := image.Decode(reader)
 	if decodeErr != nil {
 		panic(decodeErr)
 	}
 	bounds := image.Bounds()
 
+	// extract the 8-bit RGBA values of the pixels
 	imageBytes := []byte{}
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
@@ -133,6 +143,7 @@ func getTargetImage(inputImageFilePath string) TargetImage {
 	}
 }
 
+// runSimulatedAnnealing initializes the structs needed to run the simulation, and starts it
 func runSimulatedAnnealing(
 	targetImage TargetImage,
 	numSeeds int,
@@ -140,6 +151,7 @@ func runSimulatedAnnealing(
 	snapshotsInterval time.Duration,
 ) {
 
+	// create a file that logs the temperature progresses in function of time since the start of the simulation, for further analysis
 	statFile, err := os.Create(
 		fmt.Sprintf("./res/%s_%d-seeds.csv",
 			targetImage.Name,
@@ -149,10 +161,7 @@ func runSimulatedAnnealing(
 		panic(err)
 	}
 
-	ebiten.SetWindowTitle(
-		fmt.Sprintf("Voronoi Simulated Annealing - %ds", numSeeds))
-	ebiten.SetWindowSize(targetImage.Width, targetImage.Height)
-
+	// initialize the Voronoi diagram
 	voronoi, vErr := NewVoronoi(
 		targetImage.Width,
 		targetImage.Height,
@@ -162,6 +171,7 @@ func runSimulatedAnnealing(
 		panic(vErr)
 	}
 
+	// initialize the simulated annealing
 	simulatedAnnealing, saErr := NewSimulatedAnnealing(
 		voronoi,
 		targetImage,
@@ -171,6 +181,7 @@ func runSimulatedAnnealing(
 		panic(saErr)
 	}
 
+	// initialize the canvas for the GUI
 	c, cErr := NewCanvas(
 		targetImage.Name,
 		numSeeds,
@@ -184,6 +195,12 @@ func runSimulatedAnnealing(
 		panic(cErr)
 	}
 
+	// initialize the system window size and title
+	ebiten.SetWindowTitle(
+		fmt.Sprintf("Voronoi Simulated Annealing (%d seeds)", numSeeds))
+	ebiten.SetWindowSize(targetImage.Width, targetImage.Height)
+
+	// run the simulation
 	if err := ebiten.RunGame(c); err != nil {
 		if err == SimulationCompleted {
 			return
